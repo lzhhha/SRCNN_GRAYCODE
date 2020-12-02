2d8357cac285ffa5394e29f6ca6240cf011caed6
lzhhha <46672763+lzhhha@users.noreply.github.com>
2020-12-03 00:56:32 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,175 +1,151 @@
-from os import listdir
-from os.path import join
-
-from torch.utils.data import Dataset
-import torchvision.transforms as transforms
-from PIL import Image, ImageFilter
-
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
 import numpy as np
 import torch
 import cv2
-import os
+# from skimage import io
+import matplotlib.pyplot as plt
 
-import torch.nn.functional as F
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(8, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(32, 8, kernel_size=5, padding=2)
+        self.conv4 = nn.Conv2d(8, 8, kernel_size=1, padding=0)
 
-def is_image_file(filename):
-    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
+    # def forward(self, x):
+    #
+    #     # [4,1,32,32]
+    #     it = x.cuda().float()
+    #     x0 = (x.cpu().detach().data.numpy()).astype("uint8")  # x转numpy
+    #     # print(x0)
+    #     # print("x0:", x.shape)  #x0: torch.Size([4, 1, 32, 32])
+    #
+    #     x1 = np.zeros([x0.shape[0], x0.shape[2], x0.shape[3]], dtype='uint8')
+    #
+    #     # print("x1:",x1.shape)  #x1: (4, 32, 32)
+    #
+    #     for i in range(x0.shape[0]):
+    #         img = x0[i, :, :, :]
+    #         # print("img:",img.shape)  #img: (1, 32, 32)
+    #         img = np.transpose(img, (1, 2, 0))  # [h, w, 1]  transpose 作用是改变序列
+    #         # print(img.shape) #(32, 32, 1)
+    #         x1[i, :, :] = img[:, :, 0]
+    #
+    #     # print("img:", img.shape) #img: (32, 32, 1)
+    #     # print("x1:", x1.shape) #x1: (4, 32, 32)
+    #     img = x1  #img: (4, 32, 32)
+    #
+    #     # 转十进制格雷码
+    #     img_new = (img.astype("uint8") / 2).astype("uint8")
+    #     img = img.astype("uint8")
+    #     img_new = img ^ img_new
+    #     # print("img_new:", img_new.shape)  #img_new: (4, 32, 32) img_new: (2, 32, 32)
+    #
+    #     [h, w] = img_new.shape[1], img_new.shape[2]
+    #
+    #     image = np.empty((x0.shape[0], 8, h, w), dtype=np.uint8)  # 存余数 image[4,8,32,32]
+    #
+    #     for i in range(8):
+    #         image[:, i, :, :] = img_new % 2  # 转格雷码8维图像
+    #         # print(image[:, :, i])
+    #         img_new = img_new // 2
+    #
+    #     x = torch.from_numpy(np.ascontiguousarray(image)).float() #torch.from_numpy()方法把数组转换成张量，且二者共享内存，对张量进行修改比如重新赋值，那么原始数组也会相应发生改变。
+    #     x = x.cuda()    # [4,8,32,32]
+    #
+    #     # inte = x
+    #     # x /= 255
+    #     out = F.relu(self.conv1(x))     # [4,64,32,32]
+    #
+    #     # xshow = x.cpu().detach().data.numpy()[0, 0, :, :]
+    #     # out = out.cpu().detach().data.numpy()[0, 0, :, :]
+    #     # addimg = (xshow + out)/2
+    #     #
+    #     # plt.figure()
+    #     # plt.subplot(1, 3, 1)
+    #     # plt.imshow(xshow)
+    #     # plt.subplot(1, 3, 2)
+    #     # plt.imshow(out)
+    #     # plt.subplot(1, 3, 3)
+    #     # plt.imshow(addimg)
+    #     #
+    #     # plt.show()
+    #     # exit(-1)
+    #
+    #
+    #     out = F.relu(self.conv2(out))   # [4,32,32,32]
+    #     out = self.conv3(out)   # [4,8,32,32]
+    #     out = out.cuda()
+    #     # print(it.dtype)
+    #     # print(out.dtype)
+    #     out += it
+    #     # out = F.sigmoid(out)
+    #     # print(torch.mean(out))
+    #     return out
 
-def load_img(lr_path, hr_path):
-    lr = Image.open(lr_path).convert('YCbCr')
-    hr = Image.open(hr_path).convert("YCbCr")
-    y_lr, _, _ = lr.split()
-    y_hr, _, _ = hr.split()
-    return y_lr, y_hr
+    def forward(self, x, not_8):
+        # it = not_8.cuda().float()
+        # print(x.shape)
 
-def random_crop(images, split):
+        # inte = x
+        # x /= 255
 
-    h, w = images[1].shape[:2]
+        out = F.relu(self.conv1(x))     # [4,64,32,32]
 
-    crops = []
+        '''xshow = x.cpu().detach().data.numpy()[0, 0, :, :]
+        out = out.cpu().detach().data.numpy()[0, 0, :, :]
+        print(xshow)
+        print(out)
+        exit(-1)
+        addimg = (xshow + out)/2
 
-    if split == 'test':
-        new_h = 256
-        new_w = 256
+        plt.figure()
+        plt.subplot(1, 3, 1)
+        plt.imshow(xshow)
+        plt.subplot(1, 3, 2)
+        plt.imshow(out)
+        plt.subplot(1, 3, 3)
+        plt.imshow(addimg)
 
-        # for image in images:
-        #     imaget = image[0:0 + new_h, 0:0 + new_w]
-        #     crops.append(imaget)
-        lr = images[0][0:0 + (new_h // 2), 0:0 + (new_w // 2)]
-        hr = images[1][0:0 + (new_h), 0:0 + (new_w)]
+        plt.show()
+        exit(-1)'''
 
-        crops.append(lr)
-        crops.append(hr)
-    else:
-        new_h = 196
-        new_w = 196
-        y = np.random.randint(0, h-new_h)   # 随机整型数[0,h-new_h)
-        x = np.random.randint(0, w-new_w)
 
-        lr = images[0][y//2:y//2+(new_h//2), x//2:x//2+(new_w//2)]
-        hr = images[1][y:y + (new_h), x:x + (new_w)]
+        out = F.relu(self.conv2(out))   # [4,32,32,32]
+        # print(out)
+        # out = F.relu(self.conv3(out))  # [4,8,32,32]
+        out = F.relu(self.conv3(out))
+        # print(out)
 
-        crops.append(lr)
-        crops.append(hr)
-
-        # for image in images:
-        #     imaget = image[y:y+new_h, x:x+new_w]
-        #     crops.append(imaget)
-
-    return crops
-
-class DatasetFromFolder(Dataset):
-    def __init__(self, split, hr_path, lr_path, zoom_factor):
-        super(DatasetFromFolder, self).__init__()
-
-        self.hr_path = hr_path
-        self.split = split
-        self.lr_path = lr_path
-        # self.hr_filenames = [(os.listdir(hr_path)).sort(key=lambda x:int(x[:-4]))]
-        self.hr_filenames = os.listdir(hr_path)
-        # self.hr_filenames = [join(hr_path, x) for x in listdir(hr_path) if is_image_file(x)]
-        self.zoom_factor = zoom_factor
-
-        self.input_transform = transforms.Compose(transforms.Resize(196, interpolation=Image.BICUBIC))
-
-    def __getitem__(self, index):
-        # print(self.hr_filenames)
-        self.hr_filenames.sort(key=lambda x:int(x[:-4]))
-        hr_filename = self.hr_path + '/' + self.hr_filenames[index]
-        lr_filename = self.lr_path + '/' + "X" + self.zoom_factor + '/' + self.hr_filenames[index].split('.')[0] + "x" + self.zoom_factor + ".png"
-        # hr_filename = self.hr_filenames[index]
-        # lr_filename = str(os.path.dirname(self.hr_filenames[index]) +'/' + str(os.path.basename(self.hr_filenames[index]).split('.'))[0] + "x" + self.zoom_factor + ".png")
-        input, target = load_img(lr_filename, hr_filename)
-        # input = self.input_transform(input)
-
-        # lrnumpy = np.transpose(input_3.cpu().detach().data.numpy()[0], [1, 2, 0])
-        # hrnumpy = np.transpose(target.cpu().detach().data.numpy()[0], [1, 2, 0])
-        # import skimage.measure
-        #
-        # print(skimage.measure.compare_psnr(lrnumpy, hrnumpy, data_range=1))
-
-        input = np.asarray(input).astype("uint8")/255   # [702,1020]
-        target = np.asarray(target).astype("uint8")/255  # [1404,2040]
-
-        # exit(-1)
-        input, target = random_crop([input, target], self.split)
-        # print(target.shape)
-        # print(input.shape)
-
-        input = np.reshape(input, [1, input.shape[0], input.shape[1]])
-        target = np.reshape(target, [1, target.shape[0], target.shape[1]])
-
-        # a1 = np.zeros([1, input.shape[0], input.shape[1]])
-        # a2 = np.zeros([1, target.shape[0], target.shape[1]])
-        # a1[0] += input
-        # a2[0] += target
-
-        # input = np.transpose(input, (1, 2, 0))
-        # target = np.transpose(target, (1, 2, 0))
-
-        input = torch.from_numpy(np.ascontiguousarray(input)).float()
-        input_1 = input.clone()
-
-        # input_1 = torch.unsqueeze(input, dim=0)     # [1,1,h,w]
-        # print(input_1.shape)
-        # input_1 = F.upsample_bilinear(input, scale_factor=2)
-        # input_1 = input_1[0]
-        # print(input_1.shape)
-        # print(torch.mean(input))
-
-        input = self.hr_convert(input)
-
-        target = torch.from_numpy(np.ascontiguousarray(target)).float()
-
-        target_hr = target.clone()
-        target = self.hr_convert(target)  # [8, 32, 32]的tensor
-
-        # 保存8通道hr、lr图像
-        # hr_8 = (target.cpu().detach().data.numpy()).astype('uint8')
-        # sr_8 = (input.cpu().detach().data.numpy()).astype('uint8')
-        # hr_8 = np.transpose(hr_8, [1, 2, 0])  # [96,96,8]
-        # sr_8 = np.transpose(sr_8, [1, 2, 0])  # [96,96,8]
-
-        # cv2.imwrite('/export/liuzhe/program2/SRCNN/hr_8_sr_8/hr.png', hr_8*255 )
-        # cv2.imwrite('/export/liuzhe/program2/SRCNN/hr_8_sr_8/sr.png', sr_8*255 )
-        # exit(-1)
-        # for i in range(8):
-        #     cv2.imwrite('/export/liuzhe/program2/SRCNN/hr_8_sr_8/hr_{}.png'.format(i), hr_8[:, :, i] * 255)
-        #     cv2.imwrite('/export/liuzhe/program2/SRCNN/hr_8_sr_8/sr_{}.png'.format(i), sr_8[:, :, i] * 255)
+        # xshow = x.cpu().detach().data.numpy()[0, 0, :, :]
+        # out = out.cpu().detach().data.numpy()[0, 0, :, :]
+        # print(xshow)
+        # print(out)
         # exit(-1)
 
-        # print(np.max(input))
-        # print(torch.mean(input))
-        # print(torch.max(input))
+        out = out.cuda()
 
-        return input, target, target_hr, input_1    # tensor
+        # out += it
+        # out = F.interpolate(out, scale_factor=2, mode='nearest')
 
-    def __len__(self):
-        return len(self.hr_filenames)
+        # it = F.upsample_bilinear(it, scale_factor=2)
+        out = F.upsample_bilinear(out, scale_factor=2)
 
-    def hr_convert(self, hr):
-        # [1,32,32]
-        img = hr
-        img = (np.array(img)[0] * 255).astype("uint8")
-        # print(hr)
+        out = self.conv4(out)
+        # out = F.relu(out)
 
-        # img_new = img
+        # out += it
+        # out = self.conv4(out)
+        # out = F.sigmoid(out)
+        # print(torch.mean(out))
+        return out
 
-        img_new = (img.astype("uint8") / 2).astype("uint8")  # 除2取下界等于右移位运算
-        img = img.astype("uint8")
-        img_new = img ^ img_new  # 异或运算
-
-        [h, w] = img_new.shape[0], img_new.shape[1]
-
-        image = np.empty((8, h, w), dtype=np.uint8)  # 存 余数
-
-        for i in range(8):
-            image[i, :, :] = img_new % 2  # 转格雷码8维图像
-            img_new = img_new // 2
-        # print(image)
-        # print(np.mean(image))
-
-        x_finally = torch.from_numpy(np.ascontiguousarray(image)).float()  # [8,32,32]
-        # image = torch.from_numpy(np.ascontiguousarray(image)).float()
-        # x_finally = image.cuda()
-        return x_finally
+    def initialize(self):
+        for m in self.modules():
+            # 判断这一层是否为线性层，如果为线性层则初始化权值
+            if isinstance(m, nn.Conv2d):
+                nn.init.constant_(m.weight, 0)
+                # m.weight.data = 0
